ec3e3d5d6f557c678439f3cc634ee128af6f2820
qasim <qasimhasanbilgrami1@gmail.com>
2024-09-06 11:26:08 +0500

    setup api to run from docker and terminal

---
+++ .history/controllers/csv_data/station_20240906104439.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from fastapi.responses import FileResponse
+import os
+
+router = APIRouter()
+
+@router.get("/csv", response_class=FileResponse)
+def get_station_csv():
+    # Construct the file path for the CSV file
+    filename = "station.csv"  # Replace with your actual CSV filename
+    file_path = os.path.join("/app/../assets", filename)  # Correct file path
+
+    # Check if the file exists
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+
+    return FileResponse(file_path, media_type="text/csv", filename="data.csv")
---
+++ .history/controllers/csv_data/station_20240906110341.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from fastapi.responses import FileResponse
+import os
+
+router = APIRouter()
+
+@router.get("/csv", response_class=FileResponse)
+def get_station_csv():
+    # Construct the file path for the CSV file
+     base_path = os.path.dirname(__file__) 
+    filename = "station.csv"  # Replace with your actual CSV filename
+    file_path = os.path.join("../../assets", filename)  # Correct file path
+
+    # Check if the file exists
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+
+    return FileResponse(file_path, media_type="text/csv", filename="data.csv")
